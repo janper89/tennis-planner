1c4f57ce5447f99bedf16d29a0f79d482c5109bf
Jan Perutka <janperutka@Jan-MacBook-Air.local>
2026-01-06 10:47:44 +0100

    chore: Remove temporary SQL and test files

--- disable-rls-temporarily.sql
+++
@@ -1,9 +0,0 @@
--- DOČASNĚ vypni RLS pro app_user tabulku (pouze pro testování!)
--- Spusť v SQL Editoru
--- POZOR: Toto je jen pro testování, v produkci by to nemělo být!
-
-ALTER TABLE app_user DISABLE ROW LEVEL SECURITY;
-
--- Po testování znovu zapni:
--- ALTER TABLE app_user ENABLE ROW LEVEL SECURITY;
-
